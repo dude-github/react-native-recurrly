766c393ecc2009890d0c4a17de6514a73b165c4e
dude-github <dudeblogger100@gmail.com>
2026-07-13 21:18:53 +0530

    feat: Project setup,nativewind, routing, tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,24 @@
 import "@/global.css"
 import { Text, View } from "react-native";
 import {Link} from "expo-router";
+import { styled } from "nativewind"
+import {SafeAreaView as RNSafeAreaView} from 'react-native-safe-area-context';
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
     return (
-        <View className="flex-1 items-center justify-center bg-backgroud">
+        <SafeAreaView className="flex-1 bg-background p-5">
             <Text className="text-xl font-bold text-success">
                 Welcome to Native wind!
             </Text>
             <Link href="/Onboarding" className="mt-4 rounded bg-primary text-white p-4">Go to Onboarding </Link>
             <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go to Sign In </Link>
             <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go to Sign Up </Link>
-
-            <Link href="/subscriptions/spotify">Spotify Subscription</Link>
+            <Link href="/app/subscriptions/spotify">Spotify Subscription</Link>
             <Link href={{
                 pathname: "/subscriptions/[id]",
                 params: {id: "claude" },
             }}>Claude Max Subscription</Link>
-        </View>
+        </SafeAreaView>
     );
 }
